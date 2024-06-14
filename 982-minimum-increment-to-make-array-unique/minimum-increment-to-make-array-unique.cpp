cf6175a2c67bdf432233d9d6267a35ac5de2143e
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-06-14 09:44:46 +0530

    Time: 91 ms (93.70%) | Memory: 73.8 MB (16.34%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        // int n = nums.size();
+        // int cnt=0;
+        // for(int i=0;i<n-1;++i){
+        //     if(nums[i]==nums[i+1]){
+        //         nums[i+1] +=1;
+        //         cnt+=1;
+        //     }
+        //     else if(nums[i]>nums[i+1]){
+        //         cnt+= nums[i]+1 - nums[i+1];
+        //         nums[i+1] = nums[i] + 1;
+        //     }
+        // }
+        // return cnt;
         int n = nums.size();
-        int cnt=0;
-        for(int i=0;i<n-1;++i){
-            if(nums[i]==nums[i+1]){
-                nums[i+1] +=1;
-                cnt+=1;
-            }
-            else if(nums[i]>nums[i+1]){
-                cnt+= nums[i]+1 - nums[i+1];
-                nums[i+1] = nums[i] + 1;
-            }
+        int maxElement = INT_MIN;
+
+        for(auto x : nums) maxElement = max(maxElement,x);
+
+        vector<int>freqCount(n+maxElement+1,0);
+
+        for(auto x : nums) freqCount[x]++;
+
+        int minIncrement = 0;
+
+        for(int i=0;i<(n+maxElement+1);++i){
+            if(freqCount[i]<=1) continue;
+            int duplicates = freqCount[i] - 1;
+            freqCount[i+1] += duplicates;
+            freqCount[i] = 1;
+            minIncrement += duplicates;
         }
-        return cnt;
+        return minIncrement;
     }
 };
