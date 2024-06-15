2e647694f39635bceda5edb155899f0600a80233
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-06-15 20:55:05 +0530

    Time: 136 ms (75.67%) | Memory: 81.7 MB (78.96%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        // int n = profits.size();
-        // vector<pair<int,int>>v(n);
-        // for(int i=0;i<n;++i){
-        //     v[i] = {capital[i],profits[i]};
-        // }
-        // sort(v.begin(),v.end());
-        // priority_queue<int>pq;
-        // while(k--){
-        //     for(int i=0;i<n;++i){
-        //         if(v[i].first<=w){
-        //             pq.push(v[i].second);
-        //         }
-        //     }
-        //     if(pq.empty()) break;
-        //     w += pq.top();
-        //     pq.pop();
-        // }
-        // return w;
-         int n = profits.size();
-        vector<pair<int, int>> projects(n);
-        for (int i = 0; i < n; i++) {
-            projects[i] = {capital[i], profits[i]};
+        int n = profits.size();
+        vector<pair<int,int>>v(n);
+        for(int i=0;i<n;++i){
+            v[i] = {capital[i],profits[i]};
         }
-        //We sort the projects by their minimum capital required in ascending order because we want to consider the projects that we can afford with our current capital. By iterating over the sorted projects, we can ensure that we only consider the projects that have a minimum capital requirement less than or equal to our current capital.
-        sort(projects.begin(), projects.end());
-        int i = 0;
-        priority_queue<int> maximizeCapital;
-        while (k--) {
-            //The condition projects[i].first <= w checks if the minimum capital requirement of the next project is less than or equal to our current capital w. If this condition is true, we can add the project to the priority queue because we have enough capital to start the project.
-            //We use this condition to ensure that we only add the available projects that we can afford to the priority queue. By checking the minimum capital requirement of the next project before adding it to the priority queue, we can avoid adding projects that we cannot afford, and we can focus on selecting the most profitable project that we can afford with our current capital.
-            //The loop while (i < n && projects[i].first <= w) runs until we add all the available projects that we can afford to the priority queue
-            while (i < n && projects[i].first <= w) {
-                maximizeCapital.push(projects[i].second);
-                i++;
+        sort(v.begin(),v.end());
+        priority_queue<int>pq;
+        int i=0;
+        while(k--){
+            for(;i<n;++i){
+                if(v[i].first<=w){
+                    pq.push(v[i].second);
+                }
+                else break;
             }
-            if (maximizeCapital.empty())
-                break;
-            w += maximizeCapital.top();
-            maximizeCapital.pop();
+            if(pq.empty()) break;
+            w += pq.top();
+            pq.pop();
         }
         return w;
     }
