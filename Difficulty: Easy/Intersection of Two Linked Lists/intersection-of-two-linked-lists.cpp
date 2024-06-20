e3ba07628e5ac465f593fd55f7e7f7c11e69e3dc
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-06-20 10:50:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two Linked Lists/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+
+// } Driver Code Ends
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+  public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        // code here
+        // return the head of intersection list
+        unordered_map<int,bool>mp;
+        Node* ptr1 = head2;
+        while(ptr1!=NULL){
+            mp[ptr1->data] = true;
+            ptr1=ptr1->next;
+        }
+        ptr1 = head1;
+        Node* ans = new Node(-1);
+        Node* res = ans;
+        while(ptr1!=NULL){
+            if(mp[ptr1->data]==true){
+                    ans->next = new Node(ptr1->data);
+                    ans = ans->next;
+            }
+            ptr1 = ptr1->next;
+        }
+        return res->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    
+	    cin>> n;
+	    Node* head1 = inputList(n);
+	    
+	    cin>>m;
+	    Node* head2 = inputList(m);
+	    Solution obj;
+	    Node* result = obj.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
