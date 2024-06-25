0489210aa85bc8ac79ae520669505b338259b4db
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-06-25 08:47:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+        // Your code here
+        vector<int> result(num);
+        priority_queue<int,vector<int>,greater<int>>pq;
+        
+        
+        for(int i=0;i<num;++i){
+            pq.push(arr[i]);
+        }
+        int index = 0;
+        while(!pq.empty()){
+            result[index++] = pq.top();
+            pq.pop();
+        }
+        
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+	int T;
+	cin>> T;
+	
+	while (T--)
+	{
+	    int num, K;
+	    cin>>num>>K;
+	    
+	    int arr[num];
+	    for(int i = 0; i<num; ++i){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    vector <int> res = ob.nearlySorted(arr, num, K);
+	    for (int i = 0; i < res.size (); i++)
+	        cout << res[i] << " ";
+	        
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+
+// } Driver Code Ends
