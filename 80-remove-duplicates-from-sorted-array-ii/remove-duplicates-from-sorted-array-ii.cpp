e6eac66d36f0af1eb35f5e48ace8db0b52c427e8
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-07-02 10:36:06 +0530

    Time: 35 ms (6.90%) | Memory: 13.4 MB (41.80%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int size = nums.size();
+       int ele = nums[0];
+       int eleFreq = 1;
+       int flag = -1;
+       int cnt = 1;
+         cout<<ele<<" "<<eleFreq<<" "<<cnt<<endl;
+       for(int i=1;i<size;++i){
+            if(nums[i]!=ele){
+                if(flag!=-1){
+                    nums[flag] = nums[i];
+                    flag++;
+                    cnt++;
+                    ele = nums[i];
+                    eleFreq = 1;
+                }
+                else{
+                    ele = nums[i];
+                    eleFreq = 1;
+                    cnt++;
+                }
+            }
+            else{
+                if(flag==-1 && eleFreq==2){
+                    flag = i;
+                    eleFreq++;
+                }
+                else{
+                    eleFreq++;
+                    if(flag!=-1 && eleFreq<=2){
+                        nums[flag] = nums[i];
+                        flag++;
+                        cnt++;
+                    }
+                    if(flag==-1) cnt++;
+                    
+                }
+            }
+
+            cout<<ele<<" "<<eleFreq<<" "<<cnt<<endl;
+       }
+
+       return cnt;
+    }
+};
