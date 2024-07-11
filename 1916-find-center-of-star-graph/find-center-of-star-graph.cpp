821113f3168726252e31635535dd771ba0b822d5
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-07-11 15:50:09 +0530

    Time: 147 ms (29.48%) | Memory: 71.1 MB (33.95%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int maxEle = -1;
-        int maxFreq = -1,a,b;
-        unordered_map<int,int>mp;
-        for(auto x : edges){
-            a = x[0];
-            b = x[1];
-            mp[a]++;
-            mp[b]++;
-            if(mp[a]>maxFreq){
-                maxFreq = mp[a];
-                maxEle = a;
-            }
-            if(mp[b]>maxFreq){
-                maxFreq = mp[b];
-                maxEle = b;
-            }
+
+        if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1]  ){
+            return edges[0][0];
         }
-        return maxEle;
+         return edges[0][1];
     }
 };
