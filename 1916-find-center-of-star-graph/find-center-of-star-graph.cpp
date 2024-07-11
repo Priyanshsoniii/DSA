8bfd0d93f4e876e694972b53d31a47f1c54bb003
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-07-11 15:08:49 +0530

    Time: 223 ms (5.75%) | Memory: 94.7 MB (5.87%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -6,12 +6,17 @@ public:
         int maxFreq = -1;
         unordered_map<int,int>mp;
         for(auto x : edges){
-            for(auto y : x){
-                mp[y]++;
-                if(mp[y]>maxFreq){
-                    maxFreq = mp[y];
-                    maxEle = y;
-                }
+            int a = x[0];
+            int b = x[1];
+            mp[a]++;
+            mp[b]++;
+            if(mp[a]>maxFreq){
+                maxFreq = mp[a];
+                maxEle = a;
+            }
+            if(mp[b]>maxFreq){
+                maxFreq = mp[b];
+                maxEle = b;
             }
         }
         return maxEle;
