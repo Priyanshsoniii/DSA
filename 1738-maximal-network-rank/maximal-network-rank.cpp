6891496394b15dde913dcb033b4e273eb562a87f
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-07-18 11:17:17 +0530

    Time: 72 ms (51.40%) | Memory: 37.4 MB (53.96%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {   
         unordered_map<int,int>freq;
         vector<vector<int>>gr(n+1,vector<int>(n+1,-1));
-        for(auto it : gr){
-            for(auto itt : it){
-                cout<<itt<<" ";
-            }cout<<endl;
-        }
         for(auto road: roads){
             freq[road[0]]++;
             freq[road[1]]++;
-            cout<<road[0]<<" "<<road[1]<<endl;
             gr[road[0]][road[1]] = 1;
             gr[road[1]][road[0]] = 1;
         }
-
         int ans = 0;
         for(int i=0;i<n;++i){
             for(int j=i+1; j<n;++j){
@@ -26,7 +18,6 @@ public:
                 ans = max(ans,freq[i]+freq[j]);
             }
         }
-
         return ans;
     }
 };
