a66b3609d632ae80d81ac9d19783ae6132e64ff9
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-07-24 11:40:31 +0530

    Time: 23 ms (21.74%) | Memory: 21 MB (10.09%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    void dfs(int i, vector<int>adj[],vector<int>&ans){
+        for(auto &nbr : adj[i]){
+            if(nbr!=-1){
+                int temp = nbr;
+                nbr = -1;
+                dfs(temp,adj,ans);
+            }
+        }
+       ans.push_back(i);
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {  
+        sort(tickets.begin(),tickets.end());
+        vector<string>dest;
+        unordered_map<string,bool>vis;
+        for(auto it : tickets){
+            string a,b;
+            a = it[0];
+            b = it[1];
+            if(vis[a]==false && a!="JFK"){
+                dest.push_back(a);
+                vis[a] = true;
+            }
+            if(vis[b]==false && b!="JFK"){
+                dest.push_back(b);
+                vis[b] = true;
+            }
+        }
+        sort(dest.begin(),dest.end());
+        dest.insert(dest.begin(),"JFK");
+        unordered_map<string,int>mpp;
+        unordered_map<int,string>mp1;
+        int num = 0;
+        for(auto it : dest){
+            mpp[it] = num;
+            mp1[num] = it;
+            num++;
+        } 
+        int n = dest.size();
+        vector<int>adj[n];
+        for(auto it : tickets){
+            string a,b;
+            a = it[0];
+            b = it[1];
+            adj[mpp[a]].push_back(mpp[b]);
+        }
+        vector<int>ans;
+        dfs(0,adj,ans);
+        vector<string>res;
+        for(auto it : ans){
+            res.push_back(mp1[it]);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
