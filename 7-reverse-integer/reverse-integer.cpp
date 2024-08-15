9e275b42a2aada201248571db286f82831beaeb3
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2024-08-15 22:00:02 +0530

    Time: 5 ms (20.22%) | Memory: 8.4 MB (27.50%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     int reverse(int x) {
-        bool isNeg = 0;
-         long long int temp = x;
-        if(temp<0){
-            isNeg = 1;
-
-            temp*=(-1);
+        int ans = 0;
+        while(x){
+            if(ans>INT_MAX/10 || ans<INT_MIN/10) return 0;
+            ans = ans*10 + x%10;
+            x/=10;
         }
-
-        long long int revNum=0;
-        while(temp){
-            if(revNum*10<=INT_MAX)
-            revNum = 10*revNum + temp%10;
-            else return 0;
-            temp/=10;
-        }
-
-        if(isNeg){
-            revNum*=(-1);
-        } 
-        return (revNum<INT_MIN || revNum>INT_MAX)?0:revNum; 
+        return ans;
     }
 };
